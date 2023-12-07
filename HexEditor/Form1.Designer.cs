8056b657b9b42f92a3eee167b67821833c5b89fb
etar125 <116297277+etar125@users.noreply.github.com>
2023-12-07 19:08:10 +0300

    Alpha 2

---
+++ HexEditor/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace HexEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.reader = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.option = new System.Windows.Forms.ComboBox();
+            this.writer = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.reader);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Reader";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(637, 397);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(147, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Show unkown characters";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // reader
+            // 
+            this.reader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.reader.BackColor = System.Drawing.SystemColors.Control;
+            this.reader.DetectUrls = false;
+            this.reader.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.reader.HideSelection = false;
+            this.reader.Location = new System.Drawing.Point(8, 6);
+            this.reader.Name = "reader";
+            this.reader.ReadOnly = true;
+            this.reader.Size = new System.Drawing.Size(776, 381);
+            this.reader.TabIndex = 1;
+            this.reader.Text = "";
+            this.reader.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(8, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(623, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.option);
+            this.tabPage2.Controls.Add(this.writer);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Writer";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // option
+            // 
+            this.option.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.option.FormattingEnabled = true;
+            this.option.Items.AddRange(new object[] {
+            "HEX",
+            "DEC",
+            "TEXT"});
+            this.option.Location = new System.Drawing.Point(8, 395);
+            this.option.Name = "option";
+            this.option.Size = new System.Drawing.Size(586, 21);
+            this.option.TabIndex = 4;
+            // 
+            // writer
+            // 
+            this.writer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.writer.BackColor = System.Drawing.SystemColors.Control;
+            this.writer.DetectUrls = false;
+            this.writer.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.writer.HideSelection = false;
+            this.writer.Location = new System.Drawing.Point(8, 7);
+            this.writer.Name = "writer";
+            this.writer.Size = new System.Drawing.Size(776, 381);
+            this.writer.TabIndex = 3;
+            this.writer.Text = "";
+            this.writer.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(600, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "All files (*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "All files (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "HexEditor";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox reader;
+        private System.Windows.Forms.RichTextBox writer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox option;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ HexEditor/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HexEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string chr = " 1234567890-=+_!@#$%^&*(){}[];:'\"\\|/?.>,<~`qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                reader.Text = "";
+                string hex = "";
+                string nor = "";
+                string txt = "";
+                StreamReader a = new StreamReader(openFileDialog1.FileName);
+                string s;
+                while(true)
+                {
+                    s = a.ReadLine();
+                    if (s != null)
+                    {
+                        foreach(char b in s)
+                        {
+                            hex += ((int)b).ToString("X") + " ";
+                            nor += ((int)b).ToString() + " ";
+                            if (checkBox1.Checked)
+                                if (chr.Contains(b))
+                                    txt += b;
+                            else
+                                txt += b;
+                        }
+                        reader.Text += "HEX: " + hex + " NORMAL: " + nor + " TEXT: " + txt + "\n";
+                        hex = "";
+                        nor = "";
+                        txt = "";
+                    }
+                    else
+                        break;
+                }
+                a.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                List<string> result = new List<string> { };
+                string temp = "";
+                string opt = option.Text;
+                foreach (string s in writer.Lines)
+                {
+                    if (opt == "HEX")
+                    {
+                        string[] a = s.Split(' ');
+                        foreach(string b in a)
+                            temp += (char)Convert.ToInt32(b, 16);
+                    }
+                    else if (opt == "DEC")
+                    {
+                        string[] a = s.Split(' ');
+                        foreach (string b in a)
+                            temp += (char)(int.Parse(b));
+                    }
+                    else if (opt == "TEXT")
+                    {
+                        temp += s;
+                    }
+                    result.Add(temp);
+                    temp = "";
+                }
+                File.WriteAllLines(saveFileDialog1.FileName, result.ToArray());
+            }
+        }
+    }
+}
